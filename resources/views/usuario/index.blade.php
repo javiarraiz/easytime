a7fbd6d2e7c5dcfd8c1a14088649ed1fff958fef
Javier <javiarraiz@gmail.com>
2025-09-22 21:23:18 -0500

    "Data Tables"
    Luego de todas las reparaciones.

--- resources/views/usuario/index.blade.php
+++ resources/views/usuario/index.blade.php
@@ -9,8 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <h1 class="text-lg font-bold mb-4">Datos usuarios</h1>
-
-                <table class="table-auto w-full border border-gray-300">
+                   
+                <table id="usuarios" class="display" style="width:100%">
                     <thead class="bg-gray-100">
                         <tr>
                             <th class="px-4 py-2 border">Número de Documento</th>
@@ -36,8 +36,32 @@
                         @endforeach
                     </tbody>
                 </table>
-
             </div>
         </div>
     </div>
+
+    {{-- jQuery + DataTables (CDN) --}}
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.8/css/jquery.dataTables.min.css">
+    <link rel="stylesheet"
+    href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+    <script>
+        $(function() {
+            $('#usuarios').DataTable({
+                pageLength: 20,
+                dom: 'Bfrtip',
+                language: {
+                    url: 'https://cdn.datatables.net/plug-ins/1.13.8/i18n/es-ES.json'
+                },
+                buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
+            });
+        });
+    </script>
 </x-app-layout>
